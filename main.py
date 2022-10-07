25caff4818cc9ae25aaff3fb3bab5355c72b2bf1
AyatoWatanuki <pomupomupurintyu@gmail.com>
2022-10-08 05:24:18 +0900

    1st commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 #2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-json_path = 'E://Patinko//App//kitaichi-fd2dd716665b.json'
+json_path = 'kitaichi-fd2dd716665b.json'
 #認証情報設定
 #ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
 credentials = ServiceAccountCredentials.from_json_keyfile_name(json_path, scope)
