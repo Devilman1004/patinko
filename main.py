014bb8fc35dc9a27a121d5f04cdd019a46b2c744
AyatoWatanuki <pomupomupurintyu@gmail.com>
2022-12-02 15:40:04 +0900

    1st commit

--- main.py
+++ main.py
@@ -20,107 +20,122 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name(json_path, scope)
 gc = gspread.authorize(credentials)
 # １．ファイル名を指定してワークブックを選択
 workbook = gc.open('収支表')
-sh_secchi = workbook.worksheet('設置一覧')
-sh_shop = workbook.worksheet('店一覧')
-sh_slot_kisyu = workbook.worksheet('スロット機種一覧')
-sh_kari = workbook.worksheet('シート7')
-df_secchi = pd.DataFrame(sh_secchi.get_all_values()[1:], columns=sh_secchi.get_all_values()[0])
-df_shop = pd.DataFrame(sh_shop.get_all_values()[1:], columns=sh_shop.get_all_values()[0])
-df_slot_kisyu = pd.DataFrame(sh_slot_kisyu.get_all_values()[1:], columns=sh_slot_kisyu.get_all_values()[0])
-df_kari = pd.DataFrame(sh_kari.get_all_values()[1:], columns=sh_kari.get_all_values()[0])
+sh = workbook.worksheet('収支')
+df = pd.DataFrame(sh.get_all_values()[1:], columns=sh.get_all_values()[0])
 
 st.title('期待値稼働')
 
-st.header('◎　目標')
-st.subheader('・年収120万円')
-st.subheader('・月収10万円')
-st.subheader('・週収2万5000円')
-st.subheader('・日収3334円')
-# st.header('◎　マイルール')
-# st.subheader('・次の日が仕事だったら２１までには帰宅')
+st.dataframe(df.tail(5))
 
 st.header('打ち始め')
-with st.form(key='nyuryoku_form'):    
-    shop_name = st.selectbox(
-        '店名',
-        tuple(df_shop['店名'])
-        )
+with st.form(key='start_form'):    
+    shop_name = st.text_input('店名')
     dai_number = st.number_input('台番号',0,10000)
-    kisyu_name = st.selectbox(
-        '機種名',
-        tuple(df_slot_kisyu['機種名'])
-        )
-    game_len = st.number_input('ゲーム数',0,20000)
+    kisyu_name = st.text_input('機種名')
+    game_len = st.number_input('開始ゲーム数',0,2000)
     kitaiti = st.number_input('期待値',0,50000)
-    replay = st.number_input('貯メダル数',0,10000)
-    
-# shop_num = list(df_shop['店番号'])[(list(df_shop['店名']).index(shop_name))]
-
-# shop_df = df_secchi[df_secchi['店番号'] == shop_num]
-
-# dai_number = st.selectbox(
-#     '台番号',
-#     tuple(shop_df['台番号'])
-#     )
-    
-# kisyu_num = shop_df['機種番号'][list(shop_df['台番号']).index(dai_number)]
-
-# kisyu_name = df_slot_kisyu['機種名'][list(df_slot_kisyu['機種番号']).index(kisyu_num)]
-
-# st.text_input('機種名',kisyu_name)
-
-# kisyu_df = df_slot_kisyu[df_slot_kisyu['機種名'] == kisyu_name].set_index('機種番号')
-
-# kisyu_df = df_kari[df_kari['機種番号'] == kisyu_num]
-
-# tenzyo_df = kisyu_df[kisyu_df['カテゴリ'] == '天井狙い']
-
-# game_list = list(tenzyo_df['項目'])
-# kitaiti_list = list(tenzyo_df['内容'])
-
-# kitaiti=int(kitaiti_list[game_list.index(str(game_len))])
-
-# st.text(str(kitaiti) + '円')
-
     touroku_buton = st.form_submit_button('登録')
-    
-    if touroku_buton:
-
-        # １．ファイル名を指定してワークブックを選択
-        sh_syushi = workbook.worksheet('収支')
-        df_syushi = pd.DataFrame(sh_secchi.get_all_values()[1:], columns=sh_secchi.get_all_values()[0])
-        
-        if list(df_syushi.index)==[]:
+    if touroku_buton: 
+        if list(df.index)==[]:
             index = 0
         else:
-            index = list(df_syushi.index)[-1]+1
+            index = int(list(df.index)[-1]+1)
         
         now = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
         day = now.date()
         start_time = now.time()
         
-        df_syushi.loc[index,'年月日'] = day
-        df_syushi.loc[index,'開始時間'] = start_time
-        df_syushi.loc[index,'店舗名'] = shop_name
-        df_syushi.loc[index,'台番号'] = dai_number
-        df_syushi.loc[index,'期待値	'] = kitaiti
+        df.loc[index,'年月日'] = day
+        df.loc[index,'開始時間'] = start_time
+        df.loc[index,'店舗名'] = shop_name
+        df.loc[index,'台番号'] = dai_number
+        df.loc[index,'機種名'] = kisyu_name
+        df.loc[index,'開始G数'] = game_len
+        df.loc[index,'期待値'] = kitaiti
         
-        set_with_dataframe(sh_syushi, df_syushi,include_index = False)
+        set_with_dataframe(sh, df, include_index = False)
         
-        st.dataframe(df_syushi.tail(5))
+        st.dataframe(df[df['年月日'] == day.strftime('%Y-%m-%d')])
+        st.text('登録完了')
+     
+     
+     
+        
+st.header('遊技中')
+with st.form(key='yugi_form'):
+    sen_mai_len = st.number_input('1000円枚数',0,500,100)
+    push_mai_len = st.number_input('1プッシュ枚数',0,50,50)
+    push_col1, push_col2 = st.columns(2)
+    with push_col1:
+        genkin_push_number = st.number_input('現金プッシュ回数',0,100)
+    with push_col2:
+        medal_push_number = st.number_input('持ちメダルプッシュ回数',0,100,1)
+        
+    endG_col1, endG_col2 = st.columns(2)
+    with endG_col1:
+        tousen_game_len = st.number_input('当選ゲーム数',0,2000)
+    with endG_col2:
+        yame_game_len = st.number_input('ヤメゲーム数',0,2000)
+    
+    yugi_touroku_buton = st.form_submit_button('登録')
+    if yugi_touroku_buton: 
+        
+        index = int(list(df.index)[-1])
+        
+        toushi_medal = (push_mai_len*genkin_push_number) + (push_mai_len*medal_push_number)
+        
+        df.loc[index,'当選G数'] = tousen_game_len
+        df.loc[index,'ヤメG数'] = yame_game_len
+        df.loc[index,'投資メダル枚数'] = toushi_medal
+        
+        one_mai = 1000 / sen_mai_len
+        if one_mai >= 20:
+            df.loc[index,'投資金額'] = (push_mai_len*medal_push_number*20) + (push_mai_len*genkin_push_number*one_mai)
+        elif 20>one_mai>=10:
+            df.loc[index,'投資金額'] = (push_mai_len*medal_push_number*10) + (push_mai_len*genkin_push_number*one_mai)
+        elif 10>one_mai>=5:
+            df.loc[index,'投資金額'] = (push_mai_len*medal_push_number*5) + (push_mai_len*genkin_push_number*one_mai)
+        elif 5>one_mai>=2:
+            df.loc[index,'投資金額'] = (push_mai_len*medal_push_number*2) + (push_mai_len*genkin_push_number*one_mai)
+        
+        set_with_dataframe(sh, df, include_index = False)
+        now = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
+        day = now.date()
+        st.dataframe(df[df['年月日'] == day.strftime('%Y-%m-%d')])
         st.text('登録完了')
 
-# with st.sidebar:
-#     kihon_df = kisyu_df[kisyu_df['カテゴリ'] == '基本情報'][['項目','内容']].set_index('項目')
 
-#     st.table(kihon_df)
 
-    # st.text('メーカー：' + kisyu_df.loc[kisyu_num,'メーカー'])
-    # st.text('タイプ：' + kisyu_df.loc[kisyu_num,'タイプ'])
-    # st.text('５０枚あたり：' + kisyu_df.loc[kisyu_num,'コイン持ち'])
-    # st.text('天井ゲーム数：' + kisyu_df.loc[kisyu_num,'天井'])
-    # st.text('純増：' + kisyu_df.loc[kisyu_num,'純増'])
+st.header('結果')
+with st.form(key='kekka_form'):
+    hyaku_mai_len = st.number_input('交換枚数')
+    kaisyu_medal = st.number_input('回収メダル枚数',0,20000)
+    
+    kekka_touroku_buton = st.form_submit_button('登録')
+    if kekka_touroku_buton: 
+        
+        index = int(list(df.index)[-1])
+        
+        now = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
+        day = now.date()
+        end_time = now.time()
+        
+        df.loc[index,'回収メダル枚数'] = kaisyu_medal
+        df.loc[index,'回収金額'] = (100/hyaku_mai_len)*kaisyu_medal
+        df.loc[index,'終了時間'] = end_time
+        
+        toushi_money = int(df.at[index,'投資金額'])
+        toushi_medal = int(df.at[index,'投資メダル枚数'])
+        kaisyuu_money = int(df.at[index,'回収金額'])
+        kaisyuu_medal = int(df.at[index,'回収メダル枚数'])
+        
+        syushi_medal = kaisyu_medal - toushi_medal
+        syushi_money = kaisyuu_money - toushi_money
+        
+        df.loc[index,'収支金額'] = syushi_money
+        df.loc[index,'収支メダル枚数'] = syushi_medal
+        
+        set_with_dataframe(sh, df, include_index = False)
 
-    # st.image('./images/sammy-rump.jpg')
-    #st.text('メーカー:' + kisyu_df.iloc[])
-
+        st.dataframe(df[df['年月日'] == day.strftime('%Y-%m-%d')])
+        st.text('登録完了')
